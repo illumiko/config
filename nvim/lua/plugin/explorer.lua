7e566b35c38fdf68a8b3240d8a7302edaec9afda
illumiko <eyanat.rayannjr@gmail.com>
2023-08-12 22:19:29 +0600

    nvim

--- nvim/lua/plugin/explorer.lua
+++ nvim/lua/plugin/explorer.lua
@@ -3,7 +3,7 @@ M.config = function()
 	vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 	require("neo-tree").setup({
 		close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
-		popup_border_style = "single",
+		popup_border_style = "rounded",
 		enable_git_status = true,
 		enable_diagnostics = true,
 		sort_case_insensitive = false, -- used when sorting files and directories in the tree
