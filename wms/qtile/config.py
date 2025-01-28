fa099cc4d856f269886d46f853ee0781a937c0d1
illumiko <eyanat.rayannjr@gmail.com>
2025-01-28 19:38:58 +0600

    zsh, qtile
    removing pycache from mods dir
    fixing ls alias

--- wms/qtile/config.py
+++ wms/qtile/config.py
@@ -39,7 +39,7 @@ elif theme == 1:
         "b1": "#8f3f71",
     }
 gap = 15
-wallpaper = "~/dotfiles/wallpaper/Wallpaper/cities/random_forest.jpg"
+wallpaper = "~/dotfiles/wallpaper/Wallpaper/steinsGate/6.jpg"
 default_padding = 10
 
 default_bg = "#222222"
